501b14ea63bc8b5501c101c85e1fdef7502868f6
jscbridge <jorge.sanchez@thebridgeschool.es>
2022-12-12 16:14:47 +0100

    Con env

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 //! Cargamos express y traemos express()
 const express = require("express");
 const app = express();
@@ -7,11 +8,11 @@ const db = require("./database/mongo.js");
 
 //! Enlazamos con routes
 const router = require("./routes/routes");
-const port = 5500;
+// const port = 5500;
 
 //! Las peticiones expresss las pasa a json()
 app.use(express.json());
 
 //! Localhost:5500
 app.use("/", router);
-app.listen(port, console.log("Servidor ON: " + port));
+app.listen(process.env.PORT, console.log("Servidor ON: " + process.env.PORT));
